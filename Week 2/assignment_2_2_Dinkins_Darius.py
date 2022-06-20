8e80ac1a85b9b9e3d400b41e5b0f8d2dee6ad7fc
dartell <dartell187@gmail.com>
2022-06-20 11:07:34 -0500

    Adding folders

---
+++ Week 2/assignment_2_2_Dinkins_Darius.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print('Hello World')
+
+print(2+2)
+
+print(9-3)
+
+print(4*7)
+
+print(9/3)
+
+print('first' + 'last')
+
+a = ['sauce',23, 'Bellevue',52.5]
+print(a)
+
+a.append('football')
+print(a)
+
+names = ('Arnold', 'Quaid', 'Matrix', 'Kimble')
